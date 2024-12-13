008f2655f464d664de5ac9a1118a8e4a72dc7f8a
Filippo Stocco <148588396+StoccoFilippo@users.noreply.github.com>
2024-12-12 18:18:37 -0800

    Update DPO_pLM.py

--- DPO_pLM.py
+++ DPO_pLM.py
@@ -35,31 +35,33 @@ CONFIG = {
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-target_length = 600 # Set here your target length you want to optimize for 
+target_lenght = 600 # Set here your target lenght
 
 # ---------------------------
 # Utility Functions
 # ---------------------------
 def seed_everything(seed=2003):
-    '''
+    """
     Sets random seed for reproducibility across libraries.
-    '''
+    """
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
 
-def format_sequence(sequence, ec_label):
-    '''
+
+def formatting_sequence(sequence, ec_label):
+    """
     Formats correctly the sequence as in the ZymCTRL trainset.
-    '''
+    """
     return f"{ec_label}<sep><start>{sequence}<end><|endoftext|>"
 
+
 def save_model_and_tokenizer(model, tokenizer, output_dir):
-    '''
+    """
     Saves the model and tokenizer to a specified directory.
-    '''
+    """
     os.makedirs(output_dir, exist_ok=True)
     model.save_pretrained(output_dir)
     tokenizer.save_pretrained(output_dir)
@@ -69,19 +71,21 @@ def save_model_and_tokenizer(model, tokenizer, output_dir):
 # ---------------------------
 # Dataset Generation
 # ---------------------------
-def generate_dataset(iteration_num, ec_label, mode='weighted'):
-    '''
-    Generates and preprocesses a dataset for training and evaluation. 
-    Also, scores the functions based on the scoring function defined 
-    and appends the weight to the data.
-    '''
-    # Initialize data dictionary
-    data = {"sequence": [], "seq_name": [], "weight": []}
-
-    # Read sequence data from the FASTA file
+def generate_dataset(iteration_num, ec_label, mode):
+    data = dict()
+    data = {
+        "sequence" : [],
+        "seq_name" : [],
+        "weight" : [],
+        }
+    
     with open(f"seq_gen_{ec_label}_iteration{iteration_num-1}.fasta", "r") as f:
         rep_seq = f.readlines()
 
+    with open(f"alpha_{ec_label}_TM_iteration{iteration_num-1}", "r") as f:
+        alpha_TM_score = f.readlines()
+        
+     
     sequences_rep = {}
     for line in rep_seq:
         if ">" in line:
@@ -89,20 +93,22 @@ def generate_dataset(iteration_num, ec_label, mode='weighted'):
         else:
             sequences_rep[name] = {"sequence": line.strip()}
 
-    # Generate sequence data and calculate rewards
-    for name, info in sequences_rep.items():
-        sequence = info["sequence"]
-        # the following function can be modified to use a custom scoring function
-        length_reward = math.exp(-(((len(sequence) / target_length) - 1) ** 2) / (0.5**2))  # Gaussian reward centered on 1
-
-        # Populate data dictionary
-        data["sequence"].append(format_sequence(sequence, ec_label))
-        data["seq_name"].append(name)
-        data["weight"].append(-length_reward)
-
+    for entry in alpha_TM_score:
+            name = entry.split("\t")[0]
+            TM = entry.split("\t")[2]
+            TM_norm_que = entry.split("\t")[4]
+            algn = int(entry.split("\t")[5])
+            sequence = sequences_rep[str(name)]['sequence']
+            lenght_rew = math.exp(-((((len(sequence)/578)-1)**2)/(0.5**2))) # Gaussian center on 1. The closer the ratio between len and aligment is, the higher is the reward
+            
+            print(f'logging:name:{name}:weight:{float(TM_norm_que)}:algn:{algn}:TM_score:{TM_norm_que}:sequence:{sequence}')
+            
+            data["sequence"].append(formatting_sequence(sequence, ec_label))
+            data["seq_name"].append(entry)
+            data["weight"].append(float(TM_norm_que)+(float(algn)/100))
+     
     # Convert data dictionary to a Hugging Face Dataset
     hf_dataset = Dataset.from_pandas(pd.DataFrame(data))
-
     # Prepare pairs if mode is 'paired'
     if mode == 'paired':
         hf_dataset = prepare_pairs(hf_dataset)
@@ -120,9 +126,9 @@ def generate_dataset(iteration_num, ec_label, mode='weighted'):
     return final_dataset
 
 def prepare_pairs(hf_dataset):
-    '''
-    Prepares data for the paired form of DPO.
-    '''
+    """
+    Prepare paired data from the paired form of DPO.
+    """
     # Sort the dataset by weight in descending order
     sorted_dataset = hf_dataset.sort("weight", reverse=True)
 
@@ -146,9 +152,6 @@ def prepare_pairs(hf_dataset):
 # Loss Functions
 # ---------------------------
 def log_likelihood(sequences, device, model, tokenizer):
-    '''
-    Calculates the negative log likelihood for all generated sequences.
-    '''
     
     all_neg_log_likelihood = []  # List to store loss for each sequence
 
@@ -163,9 +166,9 @@ def log_likelihood(sequences, device, model, tokenizer):
     return all_neg_log_likelihood
 
 def dpo_paired_loss(batch, model, ref_model, tokenizer, device, beta=0.1):
-    '''
+    """
     Calculates the paired DPO loss.
-    '''
+    """
     # Extract positive and negative sequences
     positive_sequence = batch["positive_sequence"]
     negative_sequence = batch["negative_sequence"]
@@ -186,30 +189,32 @@ def dpo_paired_loss(batch, model, ref_model, tokenizer, device, beta=0.1):
     return  torch.mean(loss)
     
 def dpo_weighted_loss(pi_log_likelihood, ref_log_likelihood, weights, beta=0.1):
-    '''
-    Calculates the Direct Preference Optimization (DPO) weighted loss. 
+    """
+    Calculates DPO weighted loss. 
     Function kindly provided by Widatalla et.al 2024 "Aligning protein 
     generative models with experimental fitness via Direct Preference Optimization"
-    '''
+    """
     if ref_log_likelihood is None:
         pi_ratio = beta * pi_log_likelihood
     else:
-        beta * (pi_log_likelihood - ref_log_likelihood)
-    
+        pi_ratio = beta * (pi_log_likelihood - ref_log_likelihood)
+        
     weights = torch.softmax(weights * -1, dim=0)
     loss = F.cross_entropy(pi_ratio, weights)
     
     return loss
 
+
 def dpo_ranked_loss(pi_log_likelihood, pi_ref_loglikelihood, weights, beta=0.1):
-    '''
-    Calculates the Direct Preference Optimization (DPO) ranked loss.
+    """
+    Calculates the Dynamic Policy Optimization (DPO) ranked loss.
     In this case the ranking is on the batch dimension.
-    '''
+
+    """
     # Sort weights and corresponding log probabilities in descending order
     sorted_indices = torch.argsort(weights.squeeze(), descending=True)
     pi_log_likelihood = pi_log_likelihood[sorted_indices]
-    pi_ref_loglikelihood = pi_ref_loglikelihood[sorted_indices] if pi_ref_loglikelihood is not None else None
+    pi_ref_loglikelihood = pi_ref_loglikelihood[sorted_indices] if ref_log_probs is not None else None
     weights = weights[sorted_indices]
     print(f"Sorted weights: {weights}")
 
@@ -226,9 +231,9 @@ def dpo_ranked_loss(pi_log_likelihood, pi_ref_loglikelihood, weights, beta=0.1):
 # Training and Evaluation
 # ---------------------------
 def train(model, ref_model, tokenizer, train_loader, optimizer, device, mode):
-    '''
+    """
     Performs training for one epoch.
-    '''
+    """
     model.train()
     total_loss = []
     for batch in train_loader:
@@ -258,27 +263,29 @@ def train(model, ref_model, tokenizer, train_loader, optimizer, device, mode):
 
     return sum(total_loss) / len(total_loss)
 
+
 def evaluate(model, ref_model, tokenizer, eval_loader, optimizer, device, mode):
-    '''
+    """
     Evaluates the model on the evaluation set.
-    '''
+    """
     model.eval()
     total_loss = []
-    for batch in eval_loader:
-        if mode != 'paired':
-            optimizer.zero_grad()
-            sequences = batch["sequence"] 
-            ref_log_probs = log_likelihood(sequences, device, ref_model, tokenizer)
-            policy_log_probs = log_likelihood(sequences, device, model, tokenizer)
-            weights = batch["weight"].to(device)
-            
-            if mode == "weighted":
-                loss = dpo_weighted_loss(policy_log_probs, ref_log_probs, weights, CONFIG["beta"])
-            
-            if mode == "ranked":
-                loss = dpo_ranked_loss(policy_log_probs, ref_log_probs, weights, CONFIG["beta"])
+    with torch.no_grad():
+        for batch in eval_loader:
+            if mode != 'paired':
+                optimizer.zero_grad()
+                sequences = batch["sequence"] 
+                ref_log_probs = log_likelihood(sequences, device, ref_model, tokenizer)
+                policy_log_probs = log_likelihood(sequences, device, model, tokenizer)
+                weights = batch["weight"].to(device)
                 
-        elif mode == "paired":
+                if mode == "weighted":
+                    loss = dpo_weighted_loss(policy_log_probs, ref_log_probs, weights, CONFIG["beta"])
+                
+                if mode == "ranked":
+                    loss = dpo_ranked_loss(policy_log_probs, ref_log_probs, weights, CONFIG["beta"])
+                
+        if mode == "paired":
             loss = dpo_paired_loss(batch, model, ref_model, tokenizer, device, CONFIG["beta"])
         
         total_loss.append(loss.item())
@@ -292,13 +299,12 @@ def evaluate(model, ref_model, tokenizer, eval_loader, optimizer, device, mode):
 # Main Function
 # ---------------------------
 def main(train_loader, eval_loader, iteration_num, model_directory, mode):
-    '''
-    Main training loop for a given iteration. Align the model to the new feedback points for new iteration.
-    '''
+    """
+    Main training loop for a given iteration.
+    """
     model_name = model_directory if iteration_num == 1 else f"output_iteration{iteration_num - 1}"
     print(f"Model {model_name} has been loaded")
 
-    # load tokenizer and both (previous) reference
     tokenizer = AutoTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
     model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
     ref_model = AutoModelForCausalLM.from_pretrained(model_directory).to(device)
@@ -318,7 +324,6 @@ def main(train_loader, eval_loader, iteration_num, model_directory, mode):
 
         save_model_and_tokenizer(model, tokenizer, output_dir=f"output_iteration{iteration_num}")
 
-    # free memory resources 
     del model
     del ref_model
     torch.cuda.empty_cache()
@@ -327,32 +332,22 @@ def main(train_loader, eval_loader, iteration_num, model_directory, mode):
 #     MAIN
 # ---------------------------
 if __name__ == "__main__":
-
-    # parse the script arguments 
     parser = argparse.ArgumentParser()
     parser.add_argument("--iteration_num", type=int, required=True)
-    parser.add_argument("--ec_label", type=str, required=True)
+    parser.add_argument("--label", type=str, required=True)
     parser.add_argument("--model_dir", type=str, required=True)
     parser.add_argument("--mode", type=str, required=True)
+
     args = parser.parse_args()
-    ec_label = args.ec_label.strip()
-    iteration_num = args.iteration_num
-    mode = args.mode
-    model_dir = args.model_dir
-    
-    # establish seed 
     seed_everything(CONFIG["seed"])
 
-    # create and save dataset or load it from previous checkpoint
-    if not os.path.exists(f"dataset_iteration{iteration_num}"):
-        dataset = generate_dataset(iteration_num, ec_label, mode)
+    if not os.path.exists(f"dataset_iteration{args.iteration_num}"):
+        dataset = generate_dataset(args.iteration_num, args.label.strip(), args.mode)
     else:
-        dataset = load_from_disk(f"dataset_iteration{iteration_num}")
-    print("Dataset Loaded!")
+        dataset = load_from_disk(f"dataset_iteration{args.iteration_num}")
 
-    # create train and eval dataset 
+    print("Dataset Loaded!")
     train_loader = DataLoader(dataset["train"], batch_size=CONFIG["batch_size"], shuffle=True)
     eval_loader = DataLoader(dataset["eval"], batch_size=CONFIG["batch_size"], shuffle=False)
 
-    # execute DPO_pLM with loaded dataset
-    main(train_loader, eval_loader, iteration_num, model_dir, mode)
+    main(train_loader, eval_loader, args.iteration_num, args.model_dir, args.mode)
